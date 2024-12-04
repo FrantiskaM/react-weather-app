4eb65565e3184e06f7098b0a4e2b54f1eca0cfca
FrantiskaM <frantiska.malucka@gmail.com>
2024-12-04 19:02:38 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by FrantiskaM and it is hosted on{" "}
+        <a
+          href="https://github.com/FrantiskaM/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
