0651c45e372955237387325ada37ccf135c03f93
FrantiskaM <frantiska.malucka@gmail.com>
2024-12-04 19:48:10 +0100

    Create application structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by FrantiskaM and it is hosted on{" "}
-        <a
-          href="https://github.com/FrantiskaM/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by FrantiskaM and it is hosted on{" "}
+          <a
+            href="https://github.com/FrantiskaM/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
